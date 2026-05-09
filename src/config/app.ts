498d353f821a91f6e89c169f4995368b0951b2c2
JonatasPablo <jonathaspabllo@gmail.com>
2026-05-09 17:03:12 -0300

    Atualiza fonte global do sistema

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = 'Lista de Tarefas'
 export const APP_DEVELOPER = 'Jonatas Pablo'
 export const DEVELOPER_EMAIL = 'jonathaspabllo@gmail.com'
-export const APP_VERSION = '2.0.16'
+export const APP_VERSION = '2.0.17'
 
 /*
 Regras da versão:
