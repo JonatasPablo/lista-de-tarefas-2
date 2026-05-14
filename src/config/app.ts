f51a6a639d2d4f684f1513ff905e2dacba6a5a12
JonatasPablo <jonathaspabllo@gmail.com>
2026-05-13 21:35:37 -0300

    Adiciona nome do app ao lado do logo no header interno - v.2.0.28

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = 'Lista de Tarefas'
 export const APP_DEVELOPER = 'Jonatas Pablo'
 export const DEVELOPER_EMAIL = 'jonathaspabllo@gmail.com'
-export const APP_VERSION = '2.0.27'
+export const APP_VERSION = '2.0.28'
 
 /*
 Regras da versão:
